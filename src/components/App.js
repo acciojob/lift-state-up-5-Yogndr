84ed006284128350cf4972a07d37571e8599ef27
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-07-27 23:44:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import './../styles/App.css';
-import Child from "./child";
+import Child from "./Child";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -12,7 +12,7 @@ const App = () => {
 
   return (
     <div>
-    <h1>Parent Component<h1/>
+      <h1>Parent Component</h1>
       {isLoggedIn ? (
         <p>You are logged in!</p>
       ) : (
