457d5b8bd9aaeb43bfaaabb77ffab58ae82136c0
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-07-27 23:07:54 +0530

    Update Child.jsx

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 const Child = ({ onLogin }) => {
   return (
-    <div>
+    <>
+      {/* <h1>Parent Component</h1> */}
       <label htmlFor="name">Username:</label>
       <input type="text" id="name" />
-      <br />
+      {/* <br /> */}
       <label htmlFor="pass">Password:</label>
       <input type="password" id="pass" />
-      <br />
-      <button onClick={onLogin}>Login</button>
-    </div>
+      {/* <br /> */}
+      <button onClick={()=>onLogin()}>Login</button>
+    </>
   );
 };
 
