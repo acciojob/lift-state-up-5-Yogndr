56b99ae6b2fcc459e65857a2c39312309c42a4bd
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-07-27 13:51:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Child from "./child";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {isLoggedIn ? (
+        <p>You are logged in!</p>
+      ) : (
+        <Child onLogin={handleLogin} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
