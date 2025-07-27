d6d6c59c649004e0b041d5facbbe3628c4cf3979
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-07-27 23:47:42 +0530

    Update Child.jsx

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -15,6 +15,7 @@ const Child = ({ onLogin }) => {
 
   return (
     <>
+      <form>
       <label htmlFor="name">Username:</label>
       <input 
         type="text" 
@@ -32,6 +33,7 @@ const Child = ({ onLogin }) => {
       />
       <br />
       <button onClick={handleLogin}>Login</button>
+        </form>
     </>
   );
 };
